33a696490be0a9cfab56ab17617bbe693f6adb6a
user1 <user1@DESKTOP-S89FP2G>
2016-06-12 12:15:36 -0400

    new change

--- FirstProject/src/com/FirstProject/PushToGit.java
+++ FirstProject/src/com/FirstProject/PushToGit.java
@@ -6,7 +6,7 @@ public class PushToGit {
 		System.out.println("First Line");
 		System.out.println("Second Line");
 System.out.println("Third Line");
-System.out.println("Third Line");	
+System.out.println("Fourth Line");	
 System.out.println("Third Line");	
 System.out.println("Third Line");	
 	
